a8b15f8ca3b1afe06593520c8733cd13b9ec70bb
amishasubedi <amisha.subedi1@gmail.com>
2022-05-13 15:42:17 -0500

    changes

---
+++ src/components/Card.js
@@ -0,0 +1,5 @@
+import "./Card.css";
+function Card(props) {
+  return <div className="card">{props.children}</div>;
+}
+export default Card;
--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -4,35 +4,29 @@ import ExpenseItem from "./ExpenseItem";
 function Expenses(props) {
   return (
     <div className="expenses">
-      <h2>Let's get started!</h2>
-      <div className="expenses-item1">
-        <ExpenseItem
-          title={props.items[0].title}
-          amount={props.items[0].amount}
-          date={props.items[0].date}
-        />
-      </div>
-      <div className="expenses-item2">
-        <ExpenseItem
-          title={props.items[1].title}
-          amount={props.items[1].amount}
-          date={props.items[1].date}
-        />
-      </div>
-      <div className="expenses-item3">
-        <ExpenseItem
-          title={props.items[2].title}
-          amount={props.items[2].amount}
-          date={props.items[2].date}
-        />
-      </div>
-      <div className="expenses-item4">
-        <ExpenseItem
-          title={props.items[3].title}
-          amount={props.items[3].amount}
-          date={props.items[3].date}
-        />
-      </div>
+      <ExpenseItem
+        title={props.items[0].title}
+        amount={props.items[0].amount}
+        date={props.items[0].date}
+      />
+
+      <ExpenseItem
+        title={props.items[1].title}
+        amount={props.items[1].amount}
+        date={props.items[1].date}
+      />
+
+      <ExpenseItem
+        title={props.items[2].title}
+        amount={props.items[2].amount}
+        date={props.items[2].date}
+      />
+
+      <ExpenseItem
+        title={props.items[3].title}
+        amount={props.items[3].amount}
+        date={props.items[3].date}
+      />
     </div>
   );
 }
