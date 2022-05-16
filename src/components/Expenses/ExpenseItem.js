33f52eb0c7d062e5f8c84604b4e9afd9bcc759e2
amishasubedi <amisha.subedi1@gmail.com>
2022-05-16 15:32:25 -0500

    changes

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,11 +1,15 @@
-import React from "react";
+import React, { useState } from "react"; // to run the component function after some changes like event handlers
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 import Card from "../UI/Card";
 
 function ExpenseItem(props) {
+  const [title, setTitle] = useState(props.title);
+
+  // click handler function
   const clickHandker = () => {
-    console.log("clicked!");
+    setTitle("Updated"); // assign new value by calling setTitle - which is our state updating function, pass new value as argument
+    console.log(title);
   };
   // must have only one root element per return statement
   return (
@@ -13,7 +17,7 @@ function ExpenseItem(props) {
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
       <button onClick={clickHandker}>Change Title</button>
