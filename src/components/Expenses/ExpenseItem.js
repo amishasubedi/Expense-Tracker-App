e6488870a5cef04709f827b14d92f25794ff390d
amishasubedi <amisha.subedi1@gmail.com>
2022-05-15 21:52:08 -0500

    changes

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -4,6 +4,9 @@ import "./ExpenseItem.css";
 import Card from "../UI/Card";
 
 function ExpenseItem(props) {
+  const clickHandker = () => {
+    console.log("clicked!");
+  };
   // must have only one root element per return statement
   return (
     // wrapping around another div
@@ -13,6 +16,7 @@ function ExpenseItem(props) {
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
+      <button onClick={clickHandker}>Change Title</button>
     </Card>
   );
 }
