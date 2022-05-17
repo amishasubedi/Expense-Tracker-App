a9868b229b7cf284f28defcb109719409fab6e42
amishasubedi <amisha.subedi1@gmail.com>
2022-05-17 13:11:02 -0500

    filter year

--- src/components/Expenses/ExpenseFilter.js
+++ src/components/Expenses/ExpenseFilter.js
@@ -3,11 +3,14 @@ import React from "react";
 import "./ExpensesFilter.css";
 
 const ExpensesFilter = () => {
+  const filterChangeHanlder = (event) => {
+    console.log(event.target.value);
+  };
   return (
     <div className="expenses-filter">
       <div className="expenses-filter__control">
         <label>Filter by year</label>
-        <select>
+        <select onChange={filterChangeHanlder}>
           <option value="2022">2022</option>
           <option value="2021">2021</option>
           <option value="2020">2020</option>
