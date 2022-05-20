0af3a95c67a4419f6ebdc2d172408e188fea6697
amishasubedi <amisha.subedi1@gmail.com>
2022-05-19 22:55:27 -0500

    conditional content

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -17,6 +17,19 @@ function Expenses(props) {
     return expense.date.getFullYear().toString() === filteredYear;
   });
 
+  let expensesContent = <p>No expenses found.</p>;
+
+  if (filteredExpenses.length > 0) {
+    expensesContent =  filteredExpenses.map((expense) => (
+      <ExpenseItem
+        key={expense.id}
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+      />
+    ))}
+  }
+
   return (
     <div>
       <Card className="expenses">
@@ -24,14 +37,10 @@ function Expenses(props) {
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
-        {filteredExpenses.map((expense) => (
-          <ExpenseItem
-            key={expense.id}
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-          />
-        ))}
+        {/* {filteredExpenses.length === 0 && <p> No expense found! </p>}
+        {filteredExpenses.length > 0 &&
+          */}
+          {expensesContent}
 
         {/* <ExpenseItem
           title={props.items[0].title}
